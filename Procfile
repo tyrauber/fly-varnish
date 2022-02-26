370ec998608bd5decfb293862a35bb851a481f84
Ty Rauber <tyrauber@mac.com>
2022-02-26 12:07:38 -0400

    Add Redis 240MB

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 varnish: /apps/fly-varnish/varnish.sh
+redis: /apps/fly-varnish/redis.sh
 dashboard: PORT=3000 node /apps/fly-varnish/dashboard.js
--- dashboard.js
+++ dashboard.js
@@ -1,12 +1,45 @@
 const express = require("express");
+const Redis = require("ioredis");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(["/", "/:name"], (req, res) => {
+const REDIS_URL = `redis://x:${process.env.REDIS_PASSWORD}@127.0.0.1:6379`;
+console.log(REDIS_URL)
+const sub = new Redis(REDIS_URL);
+const pub = new Redis(REDIS_URL);
+
+sub.psubscribe(["cache-?"], (err, count) => {
+  if (err) {
+    console.error("Failed to subscribe: %s", err.message);
+  } else {
+    console.log(
+      `Subscribed successfully! This client is currently subscribed to ${count} channels.`
+    );
+  }
+});
+
+sub.on("pmessage", (pattern, channel, message) => {
+  console.log(`Received ${message} from ${channel}`);
+});
+
+app.all('*', (req, res, next) => {
+  const channel = `cache-${1 + Math.round(Math.random())}`;
+  pub.publish(channel, JSON.stringify(req.headers))
+  if (req.method == 'GET') {
+    // ADD Key
+  } else if (req.method === 'POST'){
+    // Update Key, Value
+  } else if (['DELETE', 'POST'].includes(req.method)){
+    // Expire Key
+  }
+  next()
+})
+
+
+app.all("*", (req, res) => {
   let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
   let now = Math.round(new Date().getTime() / 1000)
-  let name = req.params?.["name"];
-  html = `<h1>${['Hello', name].filter(e =>  e).join(', ')}!</h1>`;
+  html = `<h1>Hello,</h1>`;
   html += `<p>This page is served by Express, cached by Varnish, and hosted on Fly.io.</p>`;
   html += `<p>Varnish 7.0.2 is compiled from source with Varnish Modules, including XKey.</p>`;
   html += `<p>If you curl this page, (curl -I ${fullUrl}), you will see the following response headers:<br/><br/>
